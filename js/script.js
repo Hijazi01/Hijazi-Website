c53ad9c3ccc9050d06e9e625a433dbf475ba06ad
hijazy1 <103125286+hijazy1@users.noreply.github.com>
2022-11-20 22:51:52 +0200

    update-20 11

--- js/script.js
+++ js/script.js
@@ -2,9 +2,3 @@ const iny = document.getElementsByClassName("projects");
 console.log(iny);
 
 setTimeout(() => {}, );
-
-
-$(".card").click(function () {
-    $(".card").removeClass("active");
-    $(this).addClass("active");
-  });
